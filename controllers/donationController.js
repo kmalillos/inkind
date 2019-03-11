171faee2db0d1b212c9f32ad18d5a8d5058cffc9
Kaylah Malillos <malillosk@gmail.com>
2019-03-10 22:08:15 -0600

    added donation controller and edited donation api routes

--- controllers/donationController.js
+++ controllers/donationController.js
@@ -1,5 +1,53 @@
-const db = require ("../models");
+var db = require("../models")
 
 module.exports = {
-    
-}
+
+    findAllDonations: function (req, res) {
+        // Find all Donation and return them to the user with res.json
+        db.Donation
+            .findAll({})
+            .then(function (dbDonation) {
+                console.log(`Found all Donations.`);
+                res.json(dbDonation);
+            });
+    },
+
+    findOneDonation: function (req, res) {
+        // Find one Donation with the id in req.params.id and return them to the user with res.json
+        db.Donation
+            .findOne({
+                where: {
+                    id: req.params.id
+                }
+            })
+            .then(function (dbDonation) {
+                console.log("Found: " + { dbDonation });
+                res.json(dbDonation);
+            });
+    },
+
+    createDonation: function (req, res) {
+        // Create an Donation with the data available to us in req.body
+        console.log(req.body);
+        db.Donation
+            .create(req.body)
+            .then(function (dbDonation) {
+                console.log("Posted: " + { dbDonation });
+                res.json(dbDonation);
+            });
+    },
+
+    deleteDonation: function (req, res) {
+        // Delete the Donation with the id available to us in req.params.id
+        db.Donation
+            .destroy({
+                where: {
+                    id: req.params.id
+                }
+            })
+            .then(function (dbDonation) {
+                console.log("Deleted: " + { dbDonation });
+                res.json(dbDonation);
+            });
+    }
+};
--- routes/api/donation-api.js
+++ routes/api/donation-api.js
@@ -1,64 +1,10 @@
-var db = require("../../models");
+const donationController = require("../../controllers/donationController")
 
-module.exports = function (app) {
+module.exports = function(app) {
 
-  // GET route for getting all of the donations
-  app.get("/api/donation", function (req, res) {
-    var query = {};
-    if (req.query.vendor_id) {
-      query.VendorId = req.query.vendor_id;
-    }
-    db.Donation.findAll({
-      where: query
-    }).then(function (dbDonation) {
-      console.log(`Found all donations`);
-      res.json(dbDonation);
-    });
-  });
+  app.get("/api/donation", donationController.findAllDonations);
+  app.get("/api/donation/:id", donationController.findOneDonation);
+  app.post("/api/donation", donationController.createDonation);
+  app.delete("/api/donation/:id", donationController.deleteDonation);
 
-  // Get route for retrieving a single donation
-  app.get("/api/donation/:id", function (req, res) {
-    db.Donation.findOne({
-      where: {
-        id: req.params.id
-      }
-    }).then(function (dbDonation) {
-      console.log("Found: " + {dbDonation});
-      res.json(dbDonation);
-    });
-  });
-
-  // POST route for saving a new donation
-  app.post("/api/donation", function (req, res) {
-    db.Donation.create(req.body).then(function (dbDonation) {
-      console.log("Posted: " + {dbDonation});
-      res.json(dbDonation);
-    });
-  });
-
-  // DELETE route for deleting donation
-  app.delete("/api/donation/:id", function (req, res) {
-    db.Donation.destroy({
-      where: {
-        id: req.params.id
-      }
-    }).then(function (dbDonation) {
-      console.log("Deleted: " + {dbDonation});
-      res.json(dbDonation);
-    });
-  });
-
-  // PUT route for updating donation
-  app.put("/api/donation", function (req, res) {
-    db.Donation.update(
-      req.body,
-      {
-        where: {
-          id: req.body.id
-        }
-      }).then(function (dbDonation) {
-        console.log("Updated: " + {dbDonation});
-        res.json(dbDonation);
-      });
-  });
 };
