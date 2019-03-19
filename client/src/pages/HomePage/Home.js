70cee7548fbb39c8403865a818a3519569e9b209
Kaylah Malillos <malillosk@gmail.com>
2019-03-18 21:43:07 -0600

    added photos

--- client/src/pages/HomePage/Home.js
+++ client/src/pages/HomePage/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Carousel, Container, Row, Col, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./style.css";
 
@@ -9,6 +9,44 @@ const Home = () => {
         <div>
             <h1 className="text-center">In-Kind Connections</h1>
 
+            {/* <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="holder.js/800x400?text=Second slide&bg=282c34"
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="holder.js/800x400?text=Second slide&bg=282c34"
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="holder.js/800x400?text=Third slide&bg=20232a"
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>; */}
+            
             <Container>
                 <Row>
                     <Col>
@@ -44,7 +82,7 @@ const Home = () => {
                             <Card.Body>
                                 <Card.Title>Add</Card.Title>
                                 <Card.Text>
-                                    Add a new vendor. 
+                                    Add a new vendor.
                                 </Card.Text>
                                 <Link to="/add" className="btn btn-info">Go to Add</Link>
                             </Card.Body>
